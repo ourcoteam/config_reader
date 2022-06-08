078ebee1406ea364607b3c6003b3850a17f62adc
Hosain Mohamed <shs442000@gmail.com>
2022-06-08 17:05:27 +0200

    Update init.dart

--- lib/init.dart
+++ lib/init.dart
@@ -31,6 +31,8 @@ Future<void> init({
   final config = await getConfig(staticConfig);
 
   final adMobId = config.getMap('meta')?.getMap('ads')?.getMap('google')?.get('id') ?? 'ca-app-pub-3102006508276410~8783578315';
+  final adMobIOSId = config.getMap('meta')?.getMap('ads')?.getMap('ios')?.get('id') ?? 'ca-app-pub-3102006508276410~8783578315';
+
   final facebookId = config.getMap('meta')?.getMap('socialLogin')?.getMap('facebook')?.get('id');
   final facebookName = config.getMap('meta')?.getMap('socialLogin')?.getMap('facebook')?.get('name');
   final appName = tryString(config.getMap('meta')?.getMap('app')?.get('appName'), staticConfig.get('appName'));
@@ -107,7 +109,7 @@ Future<void> init({
     facebookName: facebookName,
     reversedClientId: await getReversedClientId(),
     nSUserTrackingUsageDescription: nSUserTrackingUsageDescription,
-    adMobId: adMobId,
+    adMobId: adMobIOSId,
     deeplink: deeplink,
   );
 
