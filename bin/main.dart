6e1a87edbdfd55a2a5563476abb087f656f88cc0
Ahmed3mad <3umduck@gmail.com>
2023-07-13 12:26:14 +0300

    add local config functionality

--- bin/main.dart
+++ bin/main.dart
@@ -9,6 +9,7 @@ void main(List<String> args) {
 
   init(
     local: args.any((e) => e == '--local'),
+    localConfig: args.any((e) => e == '--local-config'),
     addToGit: !args.any((e) => e == '--no-git'),
     incrementIOS: args.any((e) => e == '--inc' || e == '--inc-ios'),
     incrementAndroid: args.any((e) => e == '--inc' || e == '--inc-android'),
