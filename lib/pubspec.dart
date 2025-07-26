58255b96704f1f16990363046b6a3a7ae81de002
2codeThemes <vbegy.info@gmail.com>
2025-07-26 19:19:33 -0300

    Update pubspec.dart

--- lib/pubspec.dart
+++ lib/pubspec.dart
@@ -5,7 +5,22 @@ Future<void> changePubspec({
   bool? remoteConfigReaderDep,
 }) async {
   final file = File('pubspec.yaml');
-  final lastAndroidVersionString = file.readAsLinesSync().firstWhere((element) => element.contains('version:'));
+  final lastAndroidVersionString = file
+      .readAsLinesSync()
+      .firstWhere((element) => element.contains('version:'));
+
+  final configReaderEntry = remoteConfigReaderDep == true
+      ? '''
+  config_reader:
+    git:
+      url: https://github.com/ourcoteam/config_reader
+      ref: config_reader_nullsafety_3.24.0
+'''
+      : '''
+  config_reader:
+    path: C:/Users/user/StudioProjects/config_reader
+''';
+
   file.writeAsStringSync('''
 name: news
 description: A new Flutter project.
@@ -32,11 +47,8 @@ dependencies:
   mobx: ^2.2.0
   flutter_mobx: ^2.2.1+1
   conditional_builder_null_safety: ^0.0.6
-  #  dart_json_mapper: ^1.5.24
-  # flutter_advanced_networkimage_2: ^2.0.1
   bloc: ^7.0.0
   flutter_bloc: ^7.2.0
-  # assets_audio_player: ^3.1.1
   just_audio: ^0.9.36
   dartz: ^0.10.1
   freezed_annotation: ^2.4.1
@@ -48,7 +60,6 @@ dependencies:
   smooth_page_indicator: ^1.1.0
   dotted_border: ^2.0.0+3
   http: ^1.2.2
-  # flutter_fawry_pay: ^0.0.6+1
   youtube_player_flutter: ^9.1.1
   font_awesome_flutter: ^10.5.0
   country_pickers: ^3.0.1
@@ -62,20 +73,16 @@ dependencies:
   shimmer: ^3.0.0
   http_interceptor: ^2.0.0
   share: ^2.0.4
-  #file_picker: ^2.0.11
   firebase_messaging: ^15.1.3
   flutter_svg: ^2.0.7
   store_redirect: ^2.0.2
   flutter_i18n: ^0.36.0
   path_provider: ^2.0.15
-#  firebase_admob: ^0.10.0+1
-  # admob_flutter: ^3.0.0
   dio: ^5.3.0
   page_transition: ^2.0.9
   flutter_staggered_grid_view: ^0.4.0
   flash: ^3.0.5+2
   flutter_share: ^2.0.0
-#  in_app_review: ^2.0.6
   implicitly_animated_reorderable_list_2: ^0.6.0
   html: ^0.15.4
   connectivity: ^3.0.6
@@ -97,7 +104,6 @@ dependencies:
   video_player: ^2.7.0
   provider: ^6.0.0
   uni_links: ^0.5.1
-  #  flutter_recaptcha_v2: ^0.1.0
   super_tooltip: ^2.0.4
   flutter_rating_bar: ^4.0.1
   chewie_audio: ^1.5.0
@@ -106,22 +112,18 @@ dependencies:
   sign_in_with_apple: ^5.0.0
   firebase_auth: ^5.5.2
   firebase_core: ^3.6.0
-#  firebase_auth_oauth: ^0.2.0
   twitter_login: ^4.0.0
   badges: ^3.1.1
   notification_permissions: ^0.6.1
   flutter_tts: ^3.7.0
   html_unescape: ^2.0.0
   flutter_highlight: ^0.7.0
-  # google_mobile_ads: ^3.0.0
   rxdart: ^0.27.0
   flutter_app_badger: ^1.5.0
   app_tracking_transparency: ^2.0.4
   google_mobile_ads: ^5.2.0
   flutter_math_fork: 0.7.3
   mailto: ^2.0.0
-  # flutter_inappwebview: ^6.1.5
-  # flutter_inappwebview: ^5.7.2+3
   flutter_statusbarcolor_ns: ^0.5.0
   lecle_system_shortcuts: ^0.0.1+1
   avoid_keyboard: ^0.3.0
@@ -129,7 +131,7 @@ dependencies:
   infinity_page_view_astro: ^1.0.0
   flutter_colorpicker: ^1.1.0
   visibility_detector: ^0.4.0+2
-  
+
 dependency_overrides:
   flutter_svg: ^2.0.7
   recaptcha_enterprise_flutter: ^18.7.0
@@ -145,15 +147,7 @@ dev_dependencies:
   rename: ^2.1.1
   change_app_package_name: ^1.1.0
   lints: ^2.1.1
-
-  ${remoteConfigReaderDep == true ? '''
-  config_reader:
-    git:
-      url: https://github.com/ourcoteam/config_reader
-      ref: config_reader_nullsafety_3.24.0''' : '''
-config_reader:
-    path: C:/Users/user/StudioProjects/config_reader'''}
-      
+$configReaderEntry
   custom_flutter_launcher_name:
     git:
       url: https://github.com/ourcoteam/custom_flutter_launcher_name
